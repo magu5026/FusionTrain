8678541b5f4f8da6f4377a5e9b5e2745f655c3a6
magu5026 <magu5026@gmail.com>
2019-03-31 18:36:58 +0200

    0.17.4 - Fixed a mistake https://mods.factorio.com/mod/FusionTrain/discussion/5c9c7514bc6a7d000d35189d

--- data-updates.lua
+++ data-updates.lua
@@ -1,10 +1,10 @@
 if not settings.startup['vanilla-order'].value then
-	data.raw['item-with-entity-data']['fusion-locomotive-mk1'].subgroup = "fusion-transport"
-	data.raw['item-with-entity-data']['fusion-locomotive-mk1'].order = "a"
-	data.raw['item-with-entity-data']['fusion-locomotive-mk2'].subgroup = "fusion-transport"
-	data.raw['item-with-entity-data']['fusion-locomotive-mk2'].order = "b"
-	data.raw['item-with-entity-data']['fusion-locomotive-mk3'].subgroup = "fusion-transport"
-	data.raw['item-with-entity-data']['fusion-locomotive-mk3'].order = "c"
+	data.raw['item-with-entity-data']['fusion-locomotive-1'].subgroup = "fusion-transport"
+	data.raw['item-with-entity-data']['fusion-locomotive-1'].order = "a"
+	data.raw['item-with-entity-data']['fusion-locomotive-2'].subgroup = "fusion-transport"
+	data.raw['item-with-entity-data']['fusion-locomotive-2'].order = "b"
+	data.raw['item-with-entity-data']['fusion-locomotive-3'].subgroup = "fusion-transport"
+	data.raw['item-with-entity-data']['fusion-locomotive-3'].order = "c"
 end
 
 if settings.startup['fusion-mk-train'].value then
@@ -14,10 +14,10 @@ end
 
 if mods['bobvehicleequipment'] then
 	if settings.startup['bobmods-vehicleequipment-enablevehiclegrids'].value then
-		data.raw['locomotive']['fusion-locomotive-mk1'].equipment_grid = "bob-locomotive"
+		data.raw['locomotive']['fusion-locomotive-1'].equipment_grid = "bob-locomotive"
 		if settings.startup['fusion-mk-train'] then
-			data.raw['locomotive']['fusion-locomotive-mk2'].equipment_grid = "bob-locomotive-2"
-			data.raw['locomotive']['fusion-locomotive-mk3'].equipment_grid = "bob-locomotive-3"
+			data.raw['locomotive']['fusion-locomotive-2'].equipment_grid = "bob-locomotive-2"
+			data.raw['locomotive']['fusion-locomotive-3'].equipment_grid = "bob-locomotive-3"
 		end
 	end
 end
@@ -25,8 +25,8 @@ end
 
 if not mods['boblogistics'] then
 	if settings.startup['fusion-mk-train'].value then
-		data.raw['cargo-wagon']['cargo-wagon'].max_speed = 3.5
-		data.raw['fluid-wagon']['fluid-wagon'].max_speed = 3.5
+		data.raw['cargo-wagon']['cargo-wagon'].max_speed = 2.4
+		data.raw['fluid-wagon']['fluid-wagon'].max_speed = 2.4
 	end	
 end
 
@@ -34,14 +34,14 @@ end
 if mods['Vehicle Wagon'] and settings.startup['fusion-mk-train'].value then
 	for _,wagon in pairs(data.raw['cargo-wagon']) do
 		if wagon.name:find("vehicle-wagon",1,true) then	
-			wagon.max_speed = 3.5
+			wagon.max_speed = 2.4
 		end
 	end
 end
 
 if mods["EvenMoreLight"] then
-	for _,train in pairs(data.raw['locomotive']) do
-		if train.name:find("fusion-locomotive-mk",1,true) then
+	for _,train in pairs(data.raw['locomotive']) do	
+		if train.name:match("^fusion%-locomotive%-%d$") then
 			train.front_light =
 				{
 					{
